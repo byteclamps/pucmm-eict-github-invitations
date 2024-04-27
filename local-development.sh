dc6c18db70a987a29586239f1e73bad8fca5f47a
Gustavo <gustavojoseh@gmail.com>
2024-04-27 02:09:58 -0300

    Update on docker-compose file

--- local-development.sh
+++ local-development.sh
@@ -1,5 +1,5 @@
 clear
 
+docker network create pucmm_eict_github_invitations
 docker container rm pucmm-eict-github-invitations -v
-docker build -t pucmm-eict-github-invitations:latest -f Dockerfile .
-docker run --name pucmm-eict-github-invitations -p 8080:8080 pucmm-eict-github-invitations:latest
+docker-compose up --build
